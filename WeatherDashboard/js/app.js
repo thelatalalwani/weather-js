51558ce73ebaa429f13099573f22979031e2feed
Lata Lalwani <297639380+thelatalalwani@users.noreply.github.com>
2026-07-04 12:53:11 +0530

    Refactor search functionality to improve user input handling and streamline event listeners

--- WeatherDashboard/js/app.js
+++ WeatherDashboard/js/app.js
@@ -40,3 +40,67 @@ const loader = document.querySelector("#loader");
 
 loader.classList.remove("hidden");
 loader.classList.add("hidden");
+
+const cityInput = document.querySelector("#cityInput");
+const searchButton = document.querySelector("#searchBtn");
+
+searchButton.addEventListener("click", function () {
+
+    console.log("Search button clicked");
+    console.log(cityInput.value);
+
+});
+
+/*let city = cityInput.value.trim();
+
+searchButton.addEventListener("click", function () {
+
+    if (city === "") {
+
+        alert("Please enter city name");
+
+        return;
+    }
+
+    cityName.textContent = cityInput.value;
+
+});
+
+cityInput.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+
+        cityName.textContent = cityInput.value;
+
+    }
+
+}); */ 
+
+// much cleaner code below
+
+function searchWeather(){
+
+    let city = cityInput.value.trim();
+
+    if(city === ""){
+
+        alert("Please enter city");
+
+        return;
+    }
+
+    cityName.textContent = city;
+
+}
+
+searchButton.addEventListener("click", searchWeather);
+
+cityInput.addEventListener("keydown", function(event){
+
+    if(event.key === "Enter"){
+
+        searchWeather();
+
+    }
+
+});
